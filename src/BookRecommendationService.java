31b7d74478c5a86e117c712caf09103d13e83a0d
Richard Bernstein <shmasmasnash@gmail.com>
2014-06-21 11:18:48 -0700

    Init

---
+++ src/BookRecommendationService.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public interface BookRecommendationService {
+	//Returns a list of up to 10 books similar to the book whose ISBN number is passed in.
+	public ArrayList<Book> getRelatedBooks(String ISBN);
+}
---
+++ src/BookRecommendationServiceTest.java
@@ -0,0 +1,9 @@
+
+public class BookRecommendationServiceTest {
+		public static void main(String[] args)
+		{
+			BookRecommendationServiceImpl test = new BookRecommendationServiceImpl();
+			//.out.println(test.getRelatedBooks("0553593714"));
+			test.getRelatedBooks("1580087892");
+		}
+}
