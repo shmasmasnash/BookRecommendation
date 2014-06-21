92993a02f35e15820b575039a9c73605bb708cf2
G. Hussain Chinoy <ghchinoy@gmail.com>
2014-06-21 13:31:20 -0700

    Removed default package

--- src/BookRecommendationService.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public interface BookRecommendationService {
-	//Returns a list of up to 10 books similar to the book whose ISBN number is passed in.
-	public ArrayList<Book> getRelatedBooks(String ISBN);
-}
--- src/BookRecommendationServiceTest.java
+++
@@ -1,9 +0,0 @@
-
-public class BookRecommendationServiceTest {
-		public static void main(String[] args)
-		{
-			BookRecommendationServiceImpl test = new BookRecommendationServiceImpl();
-			//.out.println(test.getRelatedBooks("0553593714"));
-			test.getRelatedBooks("1580087892");
-		}
-}
